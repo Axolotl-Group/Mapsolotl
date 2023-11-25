a8ae77e3cb99a85cdff3175e6364fd9c4db900b9
Matthew <cummi190@gmail.com>
2023-11-25 16:36:36 -0500

    add zipcode database and zip input field

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,41 +1,48 @@
-import React from 'react';
-import ListContainer from './ListContainer.jsx';
+import React from "react";
+import ListContainer from "./ListContainer.jsx";
+import zipCache from "../data/zipcodes.js";
 
 const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <h1>GET BUSY HIKING OR GET BUSY DOING SOMETHING ELSE</h1>
 
-      <label htmlFor='lat'>LATITUDE: </label>
+      <label htmlFor="zip">ZIP CODE: </label>
+      <input name="zip" type="text" id="zip" placeholder="Ex: 48912"></input>
+
+      <label htmlFor="lat">LATITUDE: </label>
+      <input name="lat" type="text" id="lat" placeholder="Ex: 41.8781"></input>
+      <label htmlFor="long">LONGITUDE: </label>
       <input
-        name='lat'
-        type='text'
-        id='lat'
+        name="lon"
+        type="text"
+        id="long"
+        placeholder="Ex: -87.6298"
       ></input>
-      <label htmlFor='long'>LONGITUDE: </label>
+      <label htmlFor="radius">MILE RADIUS: </label>
       <input
-        name='lon'
-        type='text'
-        id='long'
-      ></input>
-      <label htmlFor='radius'>MILE RADIUS: </label>
-      <input
-        name='radius'
-        type='text'
-        id='radius'
+        name="radius"
+        type="text"
+        id="radius"
+        placeholder="Search Radius in Miles"
       ></input>
       <button
         onClick={() =>
           fetch(
             // '/search'
-            '/search?' +
+            "/search?" +
               new URLSearchParams({
-                lat: document.getElementById('lat').value,
-                lon: document.getElementById('long').value,
+                lat:
+                  zipCache[document.getElementById("zip").value].latitude ||
+                  document.getElementById("lat").value,
+                lon:
+                  zipCache[document.getElementById("zip").value].longitude ||
+                  document.getElementById("long").value,
+                radius: document.getElementById("radius").value,
               })
           )
         }
-        id='submit'
+        id="submit"
       >
         SUBMIT
       </button>
