a7a63e4d07009077eab9a00844f91232cdd117f8
Matthew <cummi190@gmail.com>
2023-11-27 10:29:32 -0500

    styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,48 +5,44 @@ import zipCache from "../data/zipcodes.js";
 const App = () => {
   return (
     <div className="App">
-      <h1>GET BUSY HIKING OR GET BUSY DOING SOMETHING ELSE</h1>
+      <h1>GET BUSY BIKING OR GET BUSY DOING SOMETHING ELSE</h1>
+      <div className="inputs">
+        <label htmlFor="zip">ZIP CODE: </label>
 
-      <label htmlFor="zip">ZIP CODE: </label>
-      <input name="zip" type="text" id="zip" placeholder="Ex: 48912"></input>
+        <input name="zip" type="text" id="zip" placeholder="Ex: 48912"></input>
 
-      <label htmlFor="lat">LATITUDE: </label>
-      <input name="lat" type="text" id="lat" placeholder="Ex: 41.8781"></input>
-      <label htmlFor="long">LONGITUDE: </label>
-      <input
-        name="lon"
-        type="text"
-        id="long"
-        placeholder="Ex: -87.6298"
-      ></input>
-      <label htmlFor="radius">MILE RADIUS: </label>
-      <input
-        name="radius"
-        type="text"
-        id="radius"
-        placeholder="Search Radius in Miles"
-      ></input>
-      <button
-        onClick={() =>
-          fetch(
-            // '/search'
-            "/search?" +
-              new URLSearchParams({
-                lat:
-                  zipCache[document.getElementById("zip").value].latitude ||
-                  document.getElementById("lat").value,
-                lon:
-                  zipCache[document.getElementById("zip").value].longitude ||
-                  document.getElementById("long").value,
-                radius: document.getElementById("radius").value,
-              })
-          )
-        }
-        id="submit"
-      >
-        SUBMIT
-      </button>
-
+        <label htmlFor="lat">LATITUDE: </label>
+        <input name="lat" type="text" id="lat" placeholder="Optional"></input>
+        <label htmlFor="long">LONGITUDE: </label>
+        <input name="lon" type="text" id="long" placeholder="Optional"></input>
+        <label htmlFor="radius">MILE RADIUS: </label>
+        <input
+          name="radius"
+          type="text"
+          id="radius"
+          placeholder="Search Radius in Miles"
+        ></input>
+        <button
+          onClick={() =>
+            fetch(
+              // '/search'
+              "/search?" +
+                new URLSearchParams({
+                  lat:
+                    zipCache[document.getElementById("zip").value].latitude ||
+                    document.getElementById("lat").value,
+                  lon:
+                    zipCache[document.getElementById("zip").value].longitude ||
+                    document.getElementById("long").value,
+                  radius: document.getElementById("radius").value,
+                })
+            )
+          }
+          id="submit"
+        >
+          SUBMIT
+        </button>
+      </div>
       <ListContainer />
     </div>
   );
